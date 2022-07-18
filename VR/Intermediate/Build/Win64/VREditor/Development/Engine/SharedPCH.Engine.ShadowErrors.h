359372d73893f2430bed3ed8199e02866f523878
KingwayKingway <1040123009@qq.com>
2022-07-18 01:36:14 +0100

    2.5
    Implemented grappling hook.
    Overhauled motion controller pawn hierarchy and internal logic. Implemented physics for motion controller pawn, so it performs basic collisions and dynamics.
    Polished flight control stick to be more logically adaptive.
    Polished logic for weapon targeting system. More progress later.

--- VR/Intermediate/Build/Win64/VREditor/Development/Engine/SharedPCH.Engine.ShadowErrors.h
+++ VR/Intermediate/Build/Win64/VREditor/Development/Engine/SharedPCH.Engine.ShadowErrors.h
@@ -1,4 +1,4 @@
-// PCH for C:\Program Files\Epic Games\UE_4.26\Engine\Source\Runtime\Engine\Public\EngineSharedPCH.h
+// PCH for C:\Program Files (x86)\Epic Games\UE_4.26\Engine\Source\Runtime\Engine\Public\EngineSharedPCH.h
 #define IS_PROGRAM 0
 #define UE_EDITOR 1
 #define ENABLE_PGO_PROFILE 0
@@ -285,4 +285,4 @@
 #define WITH_CUSTOM_SQ_STRUCTURE 0
 #define WITH_IMMEDIATE_PHYSX 0
 #define CLOTHINGSYSTEMRUNTIMEINTERFACE_API DLLIMPORT
-#include "C:/Program Files/Epic Games/UE_4.26/Engine/Source/Runtime/Engine/Public/EngineSharedPCH.h"
+#include "C:/Program Files (x86)/Epic Games/UE_4.26/Engine/Source/Runtime/Engine/Public/EngineSharedPCH.h"
